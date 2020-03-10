b3042c2bb11dc4707f5e31a75832aed89823b48d
Lucian <lucianciubotaru2403@gmail.com>
2020-03-10 23:34:54 +0200

    Finishing the Login Form and started the SESSION()

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+include("includes/config.php");
+
+// session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+    header("Location: register.php");
+}
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,6 +22,6 @@
     <title>Welcome to Slotify</title>
 </head>
 <body>
-
+    HELLO!
 </body>
 </html>
